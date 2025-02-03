bcb116f611f4d931dd8b84f2762909cc4000e964
command_block <mtf@ik.me>
2025-02-03 22:55:31 +0800

    fish: test config output

--- modules/fish/nixos.nix
+++ modules/fish/nixos.nix
@@ -1,10 +1,19 @@
 { config, lib, ... }:
 
 {
-  options.stylix.targets.fish.enable =
-    config.lib.stylix.mkEnableTarget "Fish" true;
-
-  config = lib.mkIf (config.stylix.enable && config.stylix.targets.fish.enable) {
-    programs.fish.promptInit = import ./prompt.nix config;
+  options.stylix.targets.fish = {
+    enable = config.lib.stylix.mkEnableTarget "Fish" true;
+    enableApply = lib.mkEnableOption "apply config for Fish" // {
+      default = true;
+    };
+    config = lib.mkOption {
+      type = lib.types.attrs;
+      default = lib.mkIf (config.stylix.enable && config.stylix.targets.fish.enable) {
+        programs.fish.promptInit = import ./prompt.nix config;
+      };
+      readOnly = true;
+    };
   };
+
+  config = lib.mkIf config.stylix.targets.fish.enableApply config.stylix.targets.fish.config;
 }
